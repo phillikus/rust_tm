aec282364e6acf841187bbf9a76ca70f191e3989
Philipp <pengelmann92@googlemail.com>
2018-11-26 20:59:09 +0100

    finish tape implementation

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,16 @@ mod state;
 mod tape;
 
 fn main() {
-    println!("Hello, world!");
     let state = state::State { state_type: state::StateType::Start, id: 1 };
-    let tape = tape::Tape { alphabet: "$#".chars().collect(), head_position: 0, tape: "$|||#".chars().collect() };
+    let mut tape = tape::Tape::new("$#", "$|||#");
 
     let s : String = tape.tape.iter().collect();
-    println!("{0}", s);
+
+    tape.move_head(1);
+    tape.write('#');
+    tape.move_head(5);
+    tape.write('#');
+
+
+    println!("{0}", tape.to_string());
 }
--- src/tape.rs
+++ src/tape.rs
@@ -1,15 +1,43 @@
 pub struct Tape {
     pub alphabet: Vec<char>,
-    pub head_position: usize,
+    pub head_position: i32,
     pub tape: Vec<char>
 }
 
 impl Tape {
+    pub fn new(alphabet: &str, tape: &str) -> Tape {
+        Tape { alphabet: alphabet.chars().collect(), head_position: 0, tape: tape.chars().collect() }
+    }
+
     pub fn write(&mut self, character: char) {
         if self.head_position < 1 || !self.alphabet.contains(&character) {
-            return;
+            return
         }
 
-        self.tape[self.head_position] = character;
+        self.tape[self.head_position as usize] = character;
+    }
+
+    pub fn read(&mut self) -> char {
+        if self.head_position as usize > self.tape.len() {
+            panic!("Trying to read character at invalid position: {}", self.head_position.to_string());
+        }
+
+        self.tape[self.head_position as usize]
+    }
+
+    pub fn move_head(&mut self, inc: i32) {
+        self.head_position += inc;
+
+        if self.head_position < 0 {
+            self.head_position = 0;
+        }
+
+        if self.head_position >= self.tape.len() as i32 {
+            self.tape.push('#');
+        }
+    }
+
+    pub fn to_string(&self) -> String {
+        self.tape.iter().collect()
     }
 }
